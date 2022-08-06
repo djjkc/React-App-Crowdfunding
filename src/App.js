58471a28dc049d075b0be27cf6684e6067d7ba57
djjkc <102584654+djjkc@users.noreply.github.com>
2022-08-06 11:36:31 +1000

    Saturday tutorial 6 completed

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Switch, Route, Routes } from"react-router-dom";
+import Nav from "./components/Nav";
+import HomePage from "./pages/HomePage";
+import ProjectPage from "./pages/ProjectPage";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      Hello
+    <Router>
+    <div>
+    <Nav />
+    
+    <Routes>
+      <Route exact path="/" element={<HomePage />} />
+      <Route exact path="/project" element={<ProjectPage />} />
+    </Routes>
     </div>
+    </Router>
   );
 }
 
