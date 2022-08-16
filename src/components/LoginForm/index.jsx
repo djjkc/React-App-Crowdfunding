1949eb2f2fd7e3fd8085d8626f01d371c6eea577
djjkc <102584654+djjkc@users.noreply.github.com>
2022-08-16 19:22:40 +1000

    Update index.jsx

--- src/components/LoginForm/index.jsx
+++ src/components/LoginForm/index.jsx
@@ -1,7 +1,9 @@
 import React, { useState } from "react";
 import "./LoginForm.css";
+import { useNavigate } from 'react-router-dom'
 
 function LoginForm() {
+    const navigate = useNavigate()
     const [credentials, setCredentials] = useState({
         username: "",
         password: "",
@@ -12,21 +14,28 @@ function LoginForm() {
         setCredentials((prevCredentials) => ({
         ...prevCredentials,
         [id]: value,
-    }));
-};
+        }));
+    };
+
+    const postData = async () => {
+        const response = await fetch(`${process.env.REACT_APP_API_URL}api-token-auth/`, {
+            method: "post",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(credentials),
+        })
+
+        return response.json()
+    };
 
     const handleSubmit = (e) => {
         e.preventDefault();
         if (credentials.username && credentials.password) {
-        fetch(`${process.env.REACT_APP_API_URL}api-token-auth/`, {
-        method: "post",
-        headers: {
-            "Content-Type": "application/json",
-        },
-        body: JSON.stringify(credentials),
-        }).then((response) => {
-        console.log(response.json());
-        });
+            postData().then(data => {
+                window.localStorage.setItem('token', data.token)
+                navigate('/')
+            });
         }
     };
 
@@ -42,7 +51,7 @@ function LoginForm() {
         />
         </div>
         <div>
-        <label html For="password">Password:</label>
+        <label htmlFor="password">Password:</label>
         <input
             type="password"
             id="password"
@@ -50,7 +59,7 @@ function LoginForm() {
             onChange={handleChange}
         />
         </div>
-        <button type="submit">
+        <button type="submit" onClick={handleSubmit}>
         Login
         </button>
         </form>
