6a4da7fd229ecdc0e16afeae306aa91eb0dad42f
djjkc <102584654+djjkc@users.noreply.github.com>
2022-08-21 15:47:11 +1000

    Update index.jsx

--- src/components/Nav/index.jsx
+++ src/components/Nav/index.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import { Link } from "react-router-dom";
+// import Bernadette from 'src/components/images/Bernadette.jpg'
 
 
 function Nav() {
     return (
         <nav>
-        <Link className="Bernadette" to="/">Bernadette <img src={'src/components/images/Bernadette.jpg'} alt="Bernadette Bear Logo" width ="10px" /></Link>
+        <Link className="Bernadette" to="/">Bernadette <img src={"Bernadette"} alt="Bernadette Bear Logo" width ="10px" /></Link>
         <div className="nav-links">
         <Link to="/aboutus">About Us</Link>
         <Link to="/howwework">How We Work</Link>
